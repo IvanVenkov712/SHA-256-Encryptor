4c1ab0186f801ed030075b2ea24779d7af7adf34
Ivan Venkov <ivan.venkov.developer@gmail.com>
2023-12-29 13:38:52 +0200

    Worked on FileHelper functions

---
+++ SHA-256 Encryptor/FileHelpers.cpp
@@ -0,0 +1,51 @@
+#include "FileHelpers.h"
+
+int AlignFileSize(int val)
+{
+	/*if (val < 0) {
+		return -1;
+	}
+	int res = CHUNKSIZE-ULLSIZE;
+	while (res < val) {
+		res += CHUNKSIZE;
+	}
+	return res + ULLSIZE;*/
+	if (val < 0) {
+		return -1;
+	}
+	val += ULLSIZE;
+	return val + CHUNKSIZE - val % CHUNKSIZE;
+
+}
+
+int FileSize(const char* fileName)
+{
+	std::ifstream fStream(fileName, std::ios::binary);
+	if (!fStream.is_open()) {
+		return -1;
+	}
+	std::streampos begin = fStream.tellg();
+	fStream.seekg(0, fStream.end);
+	int n = fStream.tellg() - begin;
+	fStream.close();
+	return n;
+}
+
+Byte* LoadFileInMemory(const char* fileName, size_t& size)
+{
+	int fSize = FileSize(fileName);
+	if (fSize < 0) {
+		return nullptr;
+	}
+	int buffSize = AlignFileSize(fSize);
+	std::ifstream fStream(fileName);
+	if (!fStream.is_open()) {
+		return nullptr;
+	}
+	char* buff = new char[buffSize];
+	fStream.read(buff, fSize);
+	fStream.close();
+
+	//
+	return (Byte*)buff;
+}
---
+++ SHA-256 Encryptor/FileHelpers.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <fstream>
+
+using Byte = unsigned char;
+const int CHUNKSIZE = 64;
+const int ULLSIZE = 8;
+
+int AlignFileSize(int val);
+int FileSize(const char* fileName);
+Byte* LoadFileInMemory(const char* fileName, int& size);
