9264cf8bbbeb3b421d8fd43a042769e290d1d13f
Ivan Venkov <ivan.venkov.developer@gmail.com>
2024-01-16 17:21:08 +0200

    Moved the logic of the program from main to StartProgram

--- SHA-256 Encryptor/Main.cpp
+++ SHA-256 Encryptor/Main.cpp
@@ -22,99 +22,131 @@
 #include "Hash.h"
 
 using namespace std;
-const int STR_SIZE = 100;
+const int HASH_STR_SIZE = 100;
 const int FILE_PATH_LEN = 255;
+
+int RunProgram();
+
 void PrintInformation();
 bool GetHashOfInputMessage(char* dst, int dstSize);
 bool GetHashOfInputFile(char* dst, int dstSize);
 bool GetHashFromUserInput(char* dst, int dstSize);
 bool SaveStringInInputFile(const char* hash);
 
+int ActionsOnOptionH();
+int ActionsOnOptionC();
+int ActionsOnOptionR();
+
+//Program entry point
 int main()
+{
+	return RunProgram();
+}
+
+//Program logic here
+int RunProgram()
 {
 	PrintInformation();
 	cout << "Enter option: ";
-	char option = '\0';		
-
-	if (!GetChar(option)) {
-		cout << "Unrecognised option" << endl;
+	char option = '\0';
+	GetChar(option);
+	option = ToLowerCase(option);
+	switch (option) {
+	case 'h':
+		return ActionsOnOptionH();
+	case 'c':
+		return ActionsOnOptionC();
+	case 'r':
+		return ActionsOnOptionR();
+	case 'x':
+		return 0;
+	default:
+		cout << "Unrecoognised option" << endl;
 		return 1;
 	}
-	else if (option == 'h') {
-		char hash[STR_SIZE] = {};
+}
+int ActionsOnOptionH()
+{
+	char hash[HASH_STR_SIZE] = {};
 
-		if (!GetHashFromUserInput(hash, STR_SIZE)) {
+	if (!GetHashFromUserInput(hash, HASH_STR_SIZE)) {
+		return 1;
+	}
+	cout << "Hash code: " << hash << endl;
+	cout << "Do you want to save it in a file? (y/n): ";
+	char c = '\0';
+	GetChar(c);
+	if (c == 'y') {
+		if (!SaveStringInInputFile(hash)) {
 			return 1;
 		}
-		cout << "Hash code: " << hash << endl;
+	}
+	else if (c != 'n') {
+		cout << "Incorrect input" << endl;
+		return 1;
+	}
+	return 0;
+}
+
+int ActionsOnOptionC()
+{
+	char hash1[HASH_STR_SIZE] = {};
+	char hash2[HASH_STR_SIZE] = {};
+	cout << "Enter a hash (lower case): ";
+	cin.getline(hash1, HASH_STR_SIZE);
+	if (cin.fail() || !IsValidHashCode(hash1)) {
+		cout << "Incorrect input" << endl;
+		return 1;
+	}
+	if (!GetHashFromUserInput(hash2, HASH_STR_SIZE)) {
+		return 1;
+	}
+	bool equals = StrCmp(hash1, hash2) == 0;
+	if (equals) {
+		cout << "The hash codes are the same" << endl;
+	}
+	else {
+		cout << "The hash codes are not the same" << endl;
+	}
+	return 0;
+}
+
+int ActionsOnOptionR()
+{
+	char hash[HASH_STR_SIZE] = {};
+	cout << "Enter a hash code (lower case): ";
+	cin.getline(hash, HASH_STR_SIZE);
+	if (cin.fail() || !IsValidHashCode(hash)) {
+		cout << "Incorrect input" << endl;
+		return 1;
+	}
+	cout << "Enter max search length: ";
+	unsigned int maxLen = 0;
+	cin >> maxLen;
+	if (cin.fail()) {
+		cout << "Incorrect input" << endl;
+		return 1;
+	}
+	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	char* res = GetStringFromHash(hash, maxLen);
+	if (res == nullptr) {
+		cout << "That hash code could not be dehashed." << endl;
+		cout << "Note: Try with a bigger max search length." << endl;
+	}
+	else {
+		cout << "Dehashed message: " << res << endl;
 		cout << "Do you want to save it in a file? (y/n): ";
 		char c = '\0';
 		GetChar(c);
 		if (c == 'y') {
-			if (!SaveStringInInputFile(hash)) {
+			if (!SaveStringInInputFile(res)) {
 				return 1;
 			}
 		}
-	}
-	else if (option == 'c') {
-		char hash1[STR_SIZE] = {};
-		char hash2[STR_SIZE] = {};
-		cout << "Enter a hash (lower case): ";
-		cin.getline(hash1, STR_SIZE);
-		if (cin.fail() || !IsValidHashCode(hash1)) {
+		else if (c != 'n') {
 			cout << "Incorrect input" << endl;
 			return 1;
 		}
-		if (!GetHashFromUserInput(hash2, STR_SIZE)) {
-			return 1;
-		}
-		bool equals = StrCmp(hash1, hash2) == 0;
-		if (equals) {
-			cout << "The hash codes are the same" << endl;
-		}
-		else {
-			cout << "The hash codes are not the same" << endl;
-		}
-	}
-	else if (option == 'r') {
-		char hash[STR_SIZE] = {};
-		cout << "Enter a hash code (lower case): ";
-		cin.getline(hash, STR_SIZE);
-		if(cin.fail() || !IsValidHashCode(hash)){
-			cout << "Incorrect input" << endl;
-			return 1;
-		}
-		cout << "Enter max search length: ";
-		unsigned int maxLen = 0;
-		cin >> maxLen;
-		if (cin.fail()) {
-			cout << "Incorrect input" << endl;
-			return 1;
-		}
-		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		char* res = GetStringFromHash(hash, maxLen);
-		if (res == nullptr) {
-			cout << "That hash code could not be dehashed." << endl;
-			cout << "Note: Try with a bigger max search length." << endl;
-		}
-		else {
-			cout << "Dehashed message: " << res << endl;
-			cout << "Do you want to save it in a file? (y/n): ";
-			char c = '\0';
-			GetChar(c);
-			if (c == 'y') {
-				if (!SaveStringInInputFile(res)) {
-					return 1;
-				}
-			}
-		}
-	}
-	else if (option == 'x') {
-		return 0;
-	}
-	else {
-		cout << "Unrecognised option" << endl;
-		return 1;
 	}
 	return 0;
 }
@@ -126,6 +158,7 @@ void PrintInformation()
 	cout << "\tc - Compare the hash of a message or a file with another hash" << endl;
 	cout << "\tr - Tries to retrieve the message with a certain hash code (it may take a lot of time)" << endl;
 	cout << "\tx - Exit" << endl;
+	cout << "\tNote: The program is case-insensitive (assumes 'A' and 'a' are equal) for user input (options, and hash codes). Depends on OS whether the program is case-sensitive for file names." << endl;
 	cout << endl;
 }
 
@@ -181,11 +214,9 @@ bool GetHashFromUserInput(char* dst, int dstSize)
 	cout << "('m' or 'f'): ";
 	char hSource = '\0';
 	bool getHashSuccess = false;
-	if (!GetChar(hSource)) {
-		cout << "Incorrect input" << endl;
-		return false;
-	}
-	else if (hSource == 'm') {
+	GetChar(hSource);
+	hSource = ToLowerCase(hSource);
+	if (hSource == 'm') {
 		getHashSuccess = GetHashOfInputMessage(dst, dstSize);
 	}
 	else if (hSource == 'f') {
